96d6e414218855cfc642eb28029f2a81a1af1b61
kouki223 <kouki.1818194@gmail.com>
2025-03-13 15:41:01 +0900

    pictureカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_12_165258) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_13_064001) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+    t.string "picture"
   end
 end
